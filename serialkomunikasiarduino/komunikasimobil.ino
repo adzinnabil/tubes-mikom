3b81554f731ac67f8b68693fccf40419b2c6e5da
adzinnabil <adzinnabil02@gmail.com>
2025-01-03 10:57:23 +0700

    progress

--- serialkomunikasiarduino/komunikasimobil.ino
+++ serialkomunikasiarduino/komunikasimobil.ino
@@ -8,6 +8,8 @@ const int LowL = 7;
 const int EnableR = 10;
 const int HighR = 8;       
 const int LowR = 9;
+bool isStopped = false; 
+
 
 void setup() {
    
@@ -26,22 +28,22 @@ void setup() {
 void ke_depan() {
     digitalWrite(HighL, LOW);
     digitalWrite(LowL, HIGH);
-    analogWrite(EnableL, 75);
+    analogWrite(EnableL, 60);
 
     digitalWrite(HighR, LOW);
     digitalWrite(LowR, HIGH);
-    analogWrite(EnableR, 75);
+    analogWrite(EnableR, 40);
     delay(1000);
 }
 
 void ke_belakang() {
     digitalWrite(HighL, HIGH);
     digitalWrite(LowL, LOW);
-    analogWrite(EnableL, 75);
+    analogWrite(EnableL, 50);
 
     digitalWrite(HighR, HIGH);
     digitalWrite(LowR, LOW);
-    analogWrite(EnableR, 75);
+    analogWrite(EnableR, 50);
     delay(1000);
 }
 
@@ -52,14 +54,14 @@ void ke_kiri() {
 
     digitalWrite(HighR, LOW);
     digitalWrite(LowR, HIGH);
-    analogWrite(EnableR, 75);
+    analogWrite(EnableR, 60);
     delay(1000);
 }
 
 void ke_kanan() {
     digitalWrite(HighL, LOW);
     digitalWrite(LowL, HIGH);
-    analogWrite(EnableL, 75);
+    analogWrite(EnableL, 60);
 
     digitalWrite(HighR, LOW);
     digitalWrite(LowR, HIGH);
@@ -75,27 +77,33 @@ void stop_motor() {
     digitalWrite(HighR, LOW);
     digitalWrite(LowR, LOW);
     analogWrite(EnableR, 0);
+    delay(1000);
 }
 
 void loop() {
     char data;
     if (Serial.available() > 0) {
         data = Serial.read();  
-
-        
         if (data == '1') {
+            delay(1000);
             Serial.println("Stop");
             stop_motor();
-        } else if (data == '2') {
+            isStopped = true; 
+        } else if (data == '2' && !isStopped) { 
+            stop_motor();
             Serial.println("Kanan");
             ke_kanan();
-        } else if (data == '3') {
+        } else if (data == '3' && !isStopped) {
+            stop_motor();
             Serial.println("Kiri");
             ke_kiri();
-        } else {
+        } else if (data != '1') {
+            stop_motor(); 
             Serial.print("Data tidak dikenali: ");
             Serial.println(data);
-        } else{
+            isStopped = false; 
+        }
+    } else if (!isStopped) { 
         ke_depan();
     }
 }
